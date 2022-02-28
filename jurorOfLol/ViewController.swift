2894d7d3542f6c6796e51c9b1e01ea7982e904ba
chanseul <whckstmf@gmail.com>
2022-03-01 01:26:48 +0900

    Initial Commit

---
+++ jurorOfLol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jurorOfLol
+//
+//  Created by 찬슬조 on 2022/03/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
