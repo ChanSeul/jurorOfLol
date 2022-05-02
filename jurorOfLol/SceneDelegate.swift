5476c9e4ae615b2e0293ec1457577716bbec8be5
chanseul <whckstmf@gmail.com>
2022-05-02 20:08:10 +0900

    앱이 Resign된지 20분 이상이면, 테이블뷰 refetch, 홈버튼 중복으로 누르면 제일 위로 scrollTo

--- jurorOfLol/SceneDelegate.swift
+++ jurorOfLol/SceneDelegate.swift
@@ -27,12 +27,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        print("foreground")
 //    }
     func sceneDidBecomeActive(_ scene: UIScene) {
-        print("becomActive")
         Singleton.shared.becomeActive.accept(true)
     }
     func sceneWillResignActive(_ scene: UIScene) {
         Singleton.shared.timeStamp = Date().timeIntervalSince1970
-        print("resign")
     }
 }
 
